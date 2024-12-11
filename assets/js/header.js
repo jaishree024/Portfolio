9343408ef34607dcb0fa840313ffd96d167b7c17
jaishree024 <jaishree.go24@gmail.com>
2024-12-11 13:29:27 +0530

    push

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,13 +1,15 @@
 console.log("hey")
-burger=document.querySelector('.hamburger');
-navbar=document.querySelector('.nav')
+let burger=document.querySelector('.hamburger');
+let nav=document.querySelector('.nav');
+let circle=document.querySelector('.circle');
 var x=1;
 burger.addEventListener('click',(e)=>{
     e.preventDefault();
     console.log(x);
     x++;
     // navbar.classlist.toggle('v-resp');
-    navbar.classList.toggle('v-resp')
-    navbar.classlist.toggle('h-nav-resp');
+    nav.classList.toggle('v-resp');
+    nav.classList.toggle('h-nav-resp');
+    circle.classList.toggle('v-respc');
 })
 console.log("end")
